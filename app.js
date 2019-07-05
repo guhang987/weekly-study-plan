05644206c1d9ac387941fa554de81a8a4d34fd57
guhang <610203816@qq.com.com>
2019-07-05 20:00:13 +0800

    last

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.engine('html',require('express-art-template'))
 
 var mongoose = require('mongoose');
 
-//1.连接你的数据库
-mongoose.connect('mongodb://localhost:27017/week5', {useNewUrlParser: true});
+//1.连接数据库
+mongoose.connect('mongodb://localhost:27017/week8', {useNewUrlParser: true});
 
 //2.设计文档结构
 var Schema = mongoose.Schema;
@@ -59,7 +59,7 @@ var studentSchema = new Schema({
 // 3.将文档结构发布为模型，
 // 第一个参数传入大写名词单数字符串表示数据库名称，系统改成users；第二个参数传入架构
 var Student = mongoose.model('Student',studentSchema)
-
+var words = "..."
 app.get('/',function(req,res){
    
     Student.find({}).sort({"pronum":-1}).exec(function(err,ret){
@@ -69,8 +69,8 @@ app.get('/',function(req,res){
         {
 
             res.render('index.html',{
-                items:ret
-                
+                items:ret,
+                words:words 
             }
            
             
@@ -108,6 +108,12 @@ app.get('/new',function(req,res){
  
     res.render('new.html')
 })
+
+app.post('/str',function(req,res){
+	words = req.body.str
+	res.redirect('/')
+})
+
 app.post('/new',function(req,res){
     var html=""
 	 if(req.body.zz5){
@@ -182,7 +188,7 @@ app.post('/new',function(req,res){
         sub3:"逻辑写作",
         pronum:0,
         html:html,//html是要渲染的html表格字段
-        pro:"<div class=\"progress-bar progresss-bar-danger rogress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"0\"aria-valuemin=\"0\" aria-valuemax=\"100\" id=\"pro\" style=\"min-width: 2em;\">0%</div>"
+        pro:"<div class=\"progress-bar          progress-bar-danger progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"0\"aria-valuemin=\"0\" aria-valuemax=\"100\" id=\"pro\" style=\"min-width: 2em;\">0%</div>"
          })
 		
 	}else{
@@ -195,7 +201,7 @@ app.post('/new',function(req,res){
 	sub4:"专业课",
 	pronum:0,
         html:html,//html是要渲染的html表格字段 
-   	pro:"<div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"0\"aria-valuemin=\"0\" aria-valuemax=\"100\" id=\"pro\" style=\"min-width: 2em;\">0%</div>" 
+   	pro:"<div class=\"progress-bar         progress-bar-danger progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"0\"aria-valuemin=\"0\" aria-valuemax=\"100\" id=\"pro\" style=\"min-width: 2em;\">0%</div>" 
 	 })
 }
     admin.save(function(err,ret){
